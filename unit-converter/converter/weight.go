83fa12740063144835c1909e2879e4685ef9ae67
Akmal Dzakwan Faiz <akmal.dz.f@gmail.com>
2026-07-26 22:31:20 +0700

    feat(weight-converter): implement weight conversion functionality

---
+++ unit-converter/converter/weight.go
@@ -0,0 +1,25 @@
+package converter
+
+import "fmt"
+
+var weightToGram = map[string]float64{
+	"milligram": 0.001,
+	"gram":      1,
+	"kilogram":  1000,
+	"ounce":     28.3495,
+	"pound":     453.592,
+}
+
+func ConvertWeight(value float64, to, from string) (float64, error) {
+	fromFactor, ok1 := weightToGram[from]
+	toFactor, ok2 := weightToGram[to]
+	if !ok1 {
+		return 0, fmt.Errorf("the unit is not recognized: %v", from)
+	} else if !ok2 {
+		return 0, fmt.Errorf("the unit is not recognized: %v", to)
+	}
+
+	toGrams := fromFactor * value
+	result := toGrams / toFactor
+	return result, nil
+}
