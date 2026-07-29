f224c79005c2d2a70e9c87c8fd0735418edd33db
Akmal Dzakwan Faiz <akmal.dz.f@gmail.com>
2026-07-29 13:40:20 +0700

    chore(weather): configure environment variables and client initialization
    Integrate godotenv for environment variable management and implement
    the weather client logic to support API key authentication.
    
    - Add godotenv dependency
    - Implement weather.Client with configurable API key and timeout
    - Add .env.example and .gitignore for environment management
    - Update main entry point to load configuration and handle weather requests

--- weather-api-wrapper-service/main.go
+++ weather-api-wrapper-service/main.go
@@ -1,16 +1,37 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"weather-api-service/weather"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	if err := godotenv.Load(); err != nil {
+		fmt.Println("missing .env file")
+	}
+	apiKey, ok := os.LookupEnv("WEATHER_API_KEY")
+	if !ok {
+		log.Fatal("WEATHER_API_KEY is requried")
+	}
+	c := weather.NewClient(apiKey)
 	mux := http.NewServeMux()
 	mux.HandleFunc("GET /weather/{city}", func(w http.ResponseWriter, r *http.Request) {
-
+		city := r.PathValue("city")
+		resp, err := c.GetWeather(city)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		if err := json.NewEncoder(w).Encode(resp); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
 	})
+
 	fmt.Println("server is running on port: 8080")
 	if err := http.ListenAndServe(":8080", mux); err != nil {
 		log.Fatal(err)
--- weather-api-wrapper-service/weather/weather.go
+++ weather-api-wrapper-service/weather/weather.go
@@ -1,7 +1,36 @@
 package weather
 
+import (
+	"net/http"
+	"time"
+)
+
+type Client struct {
+	APIKey     string
+	BaseURL    string
+	HTTPClient *http.Client
+}
+
 type Response struct {
 	Temperature float64 `json:"temperature"`
 	City        string  `json:"city"`
 	Condition   string  `json:"condition"`
+}
+
+func NewClient(apiKey string) *Client {
+	return &Client{
+		APIKey:  apiKey,
+		BaseURL: "https://weather.visualcrossing.com/VisualCrossing/rest/services/timeline",
+		HTTPClient: &http.Client{
+			Timeout: 10 * time.Second,
+		},
+	}
+}
+
+func (c *Client) GetWeather(city string) (Response, error) {
+	return Response{
+		Temperature: 10,
+		City:        city,
+		Condition:   "Good",
+	}, nil
 }
